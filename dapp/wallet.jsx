b9708908a4c6740c390a3b80a354eed790da93bd
tki <tki.wroc@gmail.com>
2019-01-01 23:00:18 +0100

    mv Connection -> Web3

--- dapp/wallet.jsx
+++ dapp/wallet.jsx
@@ -1,15 +1,17 @@
-import React        from 'react'
-import ReactDOM     from 'react-dom'
-import { Provider } from 'react-redux'
-import { store }    from 'lib/store'
-import MainView     from './views/main_view'
+import React           from 'react'
+import ReactDOM        from 'react-dom'
+import { Provider }    from 'react-redux'
+import { store }       from 'lib/store'
+import Web3            from './web3'
+import MainView        from './views/main_view'
+
 
 import './wallet.css'
 
 ReactDOM.render(
   <Provider store={store}>
-    <>
+    <Web3>
       <MainView/>
-    </>
+    </Web3>
   </Provider>,
   document.body.appendChild(document.createElement('div')));
