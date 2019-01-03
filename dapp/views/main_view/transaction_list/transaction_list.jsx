f4c04a56d766bb888b95455ef60c631e99e66742
tki <tki.wroc@gmail.com>
2019-01-03 14:11:58 +0100

    display transaction list

--- dapp/views/main_view/transaction_list/transaction_list.jsx
+++ dapp/views/main_view/transaction_list/transaction_list.jsx
@@ -1,27 +1,12 @@
-import React        from 'react'
-import { connect }  from 'react-redux'
-import { updateHistory } from 'lib/store/actions'
+import React                 from 'react'
+import { connect }           from 'react-redux'
+import { asyncFetchHistory } from 'lib/store/actions'
 
 class TransactionList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
   componentDidMount() {
-    this.fetchTransactions();
+    this.props.dispatch(asyncFetchHistory(this.props.address));
   }
 
-  //TODO: fix address being always null.
-  // it needs to be fetch on connection or in rpc call action (fetchHistory)
-  // better: initialize wallet with calls to netListening, getAccountAddress and getBalance
-  fetchTransactions() {
-    if (this.props.connected && this.props.address) {
-      fetch('https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=' + this.props.address +'&page=1&offset=10&sort=desc')
-        .then(response => response.json())
-        .then(txs => this.props.dispatch(updateHistory(txs)))
-    }
-  }
-  
   isSender (tx) {
     return tx.from == this.props.address;
   }
@@ -34,30 +19,28 @@ class TransactionList extends React.Component {
   }
   
   list (txs) {
-    txs.map((tx, index) => {
-      return (
-        <div className="tx" key={index}>
-          <div className="address">
-            { this.peer(tx) }
-          </div>
-          <div className="tx-value">
-            { tx.value }
-          </div>
-          <div className="timestamp">
-            { tx.timeStamp }
-          </div>
+    return txs.map((tx, index) => (
+      <div className="tx" key={index}>
+        <div className="address">
+          { this.peer(tx) }
         </div>
-      )
-    });
+        <div className="tx-value">
+          { tx.value }
+        </div>
+        <div className="timestamp">
+          { tx.timeStamp }
+        </div>
+      </div>
+    ));
   }
 
   render () {
     return (
-      <div className="tx-list">
+      <div className="transaction-list">
         { this.list(this.props.history) }
       </div>
     )
   }
 }
 
-export default connect(({ connected, address, history }) => ({ connected, address, history }))(TransactionList);
+export default connect(({ address, history }) => ({ address, history }))(TransactionList);
