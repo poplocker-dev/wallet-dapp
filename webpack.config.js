5b2db5a38a742a76acf713ab70ea12363337f9f0
tki <tki.wroc@gmail.com>
2019-01-18 12:27:34 +0100

    rm unused assets, change site title

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,7 @@ const config = {
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      title: 'PopWallet'
+      title: 'PopLocker'
     }),
     new Dotenv()
 	]
