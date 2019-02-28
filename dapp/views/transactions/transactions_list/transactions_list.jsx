ef4e98f23d5e0e1d365bc94c6f66ae89ab6fb239
tki <tki.wroc@gmail.com>
2019-03-01 00:41:12 +0100

    better paging with lazy load and overlapping

--- dapp/views/transactions/transactions_list/transactions_list.jsx
+++ dapp/views/transactions/transactions_list/transactions_list.jsx
@@ -1,57 +1,96 @@
-import React                   from 'react'
-import { bindActionCreators }  from 'redux'
-import { connect }             from 'react-redux'
-import Transaction             from './transaction'
-import { Preloader, Bouncing } from '@poplocker/react-ui'
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Transaction from './transaction';
+import { Preloader, Bouncing } from '@poplocker/react-ui';
 
-import { asyncFetchHistory }   from 'lib/store/actions'
+import { updateHistory, upPage, downPage } from 'lib/store/actions';
 
-import './transactions_list.css'
+import './transactions_list.css';
 
 class TransactionList extends React.Component {
   componentDidMount() {
-    this.props.loadHistory(this.props.address);
-  }
-  
-  list (txs) {
-    return (
-      <Preloader value={txs} loader={Bouncing}>
-        {
-          txs && txs.map((tx, index) => (
-            <Transaction tx={ tx } key={ index }/>
-          ))
-        }
-      </Preloader>
-    )
+    this.fetchHistory(1).then(this.props.updateHistory);
   }
 
-  render () {
+  fetchHistory(page) {
+    return fetch(
+      `${process.env.ETHERSCAN_URL}&address=${this.props.address}&page=${page}`
+    )
+      .then(response => response.json())
+      .then(response => response.result);
+  }
+
+  render() {
     return (
       <div className="transactions-list">
-        <div className="scrollable">
-          { this.list(this.props.history.items) }
-        </div>
-        <button onClick={() => { this.props.next(this.props.address, this.props.history.page) }}>Next</button>
-        <button onClick={() => { this.props.prev(this.props.address, this.props.history.page) }}>Prev</button>
+        <button onClick={this.prev.bind(this)} disabled={this.prevDisabled()}>
+          Prev
+        </button>
+        <div className="pageable">{this.list()}</div>
+        <button onClick={this.next.bind(this)} disabled={this.nextDisabled()}>
+          Next
+        </button>
       </div>
-    )
+    );
+  }
+
+  list() {
+    return (
+      <Preloader value={this.props.items != null} loader={Bouncing}>
+        {this.visibleTxs(this.props.page).map((tx, index) => (
+          <Transaction tx={tx} key={index} />
+        ))}
+      </Preloader>
+    );
+  }
+
+  next() {
+    if (this.props.page == this.props.lastPage)
+      this.fetchHistory(this.props.page + 1)
+        .then(this.props.updateHistory)
+        .then(this.props.upPage);
+    else this.props.upPage();
+  }
+
+  prev() {
+    this.props.downPage();
+  }
+
+  nextDisabled() {
+    return this.cursor(this.props.page).to >= this.props.items.length;
+  }
+
+  prevDisabled() {
+    return this.props.page == 1;
+  }
+
+  visibleTxs(page) {
+    const { from, to } = this.cursor(page);
+    return this.props.items.slice(from, to);
+  }
+
+  cursor(page) {
+    const from = page == 1 ? 0 : (page - 1) * 4;
+    const to = from + 5;
+
+    return { from, to };
   }
 }
 
-const mapDispatch = (dispatch) => ({
-  loadHistory: bindActionCreators(asyncFetchHistory, dispatch),
-  next: function(addr, page) {
-    this.loadHistory(addr, page + 1)
-  },
-  prev: function(addr, page) {
-    this.loadHistory(addr, Math.max(0, page - 1))
-  }
+const mapDispatch = dispatch => ({
+  updateHistory: bindActionCreators(updateHistory, dispatch),
+  upPage: bindActionCreators(upPage, dispatch),
+  downPage: bindActionCreators(downPage, dispatch)
 });
 
 const mapStore = ({ address, history }) => ({
   address,
-  history,
+  ...history,
+  items: history.items || []
 });
 
-
-export default connect(mapStore, mapDispatch)(TransactionList);
+export default connect(
+  mapStore,
+  mapDispatch
+)(TransactionList);
