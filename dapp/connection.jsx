495931b8b56a3e4be5344a7dd7241cb9c22fa40f
tki <tki.wroc@gmail.com>
2019-03-16 13:35:25 +0100

    handle unsettled address; primitive warning

--- dapp/connection.jsx
+++ dapp/connection.jsx
@@ -1,41 +1,45 @@
 import React         from 'react'
 import { connect }   from 'react-redux'
 import { rpc }       from 'lib/rpc_calls'
-import { Bouncing }  from '@poplocker/react-ui'
+import { Bouncing, Preloader }  from '@poplocker/react-ui'
 
 class Connection extends React.Component {
   componentDidMount() {
-    this.props.dispatch(rpc.getAddress());
-    this.props.dispatch(rpc.isListening());
-    this.timer = setInterval(() => this.updatePoll(), 2000);
+    this.pollForStatus()
+    this.timer = setInterval(() => this.pollForStatus(), 2000);
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
-    this.timer = null;
   }
 
-  loadOrView () {
-    if (this.props.connection == -1) {
-      return (
-        <Bouncing/>
-      )
-    } else {
-      return this.props.children;
-    }
-  }
-
-  // TODO: move to wallet.jsx
-  updatePoll() {
+  pollForStatus() {
     this.props.dispatch(rpc.isListening());
     this.props.dispatch(rpc.getAddress());
     this.props.dispatch(rpc.getBalance());
   }
 
+  noAddress() {
+    return (
+      <div className="no-address">
+        No Address!
+      </div>
+    )
+  }
+
+  waitOrNoAddress () {
+    if (this.props.address)
+      return Bouncing;
+    else
+      return this.noAddress;
+  }
+
   render () {
     return (
       <div className="connection">
-        { this.loadOrView() }
+        <Preloader value={this.props.connection != -1} loader={this.waitOrNoAddress()}>
+          { this.props.children }
+        </Preloader>
       </div>
     )
   }
