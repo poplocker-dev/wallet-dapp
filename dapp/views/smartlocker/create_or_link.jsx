6c19d667e274e02e1cf1a842c7b0bce829eb2871
tki <tki.wroc@gmail.com>
2019-03-23 14:38:35 +0100

    move buttons row css to react-ui

--- dapp/views/smartlocker/create_or_link.jsx
+++ dapp/views/smartlocker/create_or_link.jsx
@@ -1,6 +1,9 @@
 import React       from 'react'
 import { connect } from 'react-redux'
+import { Button }  from '@poplocker/react-ui'
 import Input       from 'ui/input'
+
+import './create_or_link.css'
 
 class CreateOrLink extends React.Component {
   constructor (props) {
@@ -10,18 +13,24 @@ class CreateOrLink extends React.Component {
 
   render () {
     return (
-      <div className="subview create-or-link-form">
+      <div className="subview create-or-link">
         <div className="title">
           Connect Your Device
         </div>
-        <Input
-          autoComplete="off"
-          spellCheck="false"
-          name="smartlocker-name"
-          label="Smart Locker Name:"
-          size="32"
-          onChange={this.handleInput.bind(this)}
-          value={this.state.name} />
+        <form >
+          <Input
+            autoComplete="off"
+            spellCheck="false"
+            name="smartlocker-name"
+            label="Smart Locker Name:"
+            size="32"
+            onChange={this.handleInput.bind(this)}
+            value={this.state.name} />
+        </form>
+        <div className="buttons--2row">
+          <Button kind="alt" icon="arrow">Create Locker</Button>
+          <Button icon="arrow-up">Link Device</Button>
+        </div>
       </div>
     );
   }
