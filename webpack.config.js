72007b484c9c855a4d1d26ec810e1db179526094
gtlewis <garethtlewis@gmail.com>
2019-03-25 20:33:41 +0100

    UI improvements

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,8 @@ const config = {
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      title: 'PopLocker'
+      title: 'PopLocker',
+      favicon: __dirname + '/dapp/assets/favicon.png'
     }),
     new Dotenv()
 	]
