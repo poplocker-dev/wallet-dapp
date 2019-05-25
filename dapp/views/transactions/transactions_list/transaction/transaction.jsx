75a18f920daca66610338a57960a795a4f5ec128
gtlewis <garethtlewis@gmail.com>
2019-05-25 15:39:23 +0200

    Improve tx history reference address

--- dapp/views/transactions/transactions_list/transaction/transaction.jsx
+++ dapp/views/transactions/transactions_list/transaction/transaction.jsx
@@ -8,6 +8,9 @@ class Transaction extends React.Component {
   constructor (props) {
     super(props);
     this.state = { address: null };
+  }
+
+  componentDidMount() {
     this.resolveAddress();
   }
 
@@ -16,7 +19,7 @@ class Transaction extends React.Component {
   }
 
   async resolveAddress() {
-    let address = this.peer(this.props.tx, this.props.txListAddress.address);
+    let address = this.peer(this.props.tx, this.props.referenceAddress);
     if (address && this.props.registrar) {
       address = await this.props.registrar.resolveName(address);
     } else {
@@ -28,7 +31,7 @@ class Transaction extends React.Component {
   render () {
     return (
       <div className="transaction">
-        <Indicator direction={ this.getIndicator(this.props.tx, this.props.txListAddress.address, this.props.status) }/>
+        <Indicator direction={ this.getIndicator(this.props.tx, this.props.referenceAddress, this.props.status) }/>
         <div className="info">
           <div className="info-top">
 
